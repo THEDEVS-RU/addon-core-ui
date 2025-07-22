f1731ea29427deddfda52ed0e2e4dd46031c6dcf
dreizer <dev@qubik.dev>
2025-07-22 23:21:37 +0300

    mix

--- core-ui/src/main/java/ru/thedevs/coreui/view/unit/UnitDetailView.java
+++ core-ui/src/main/java/ru/thedevs/coreui/view/unit/UnitDetailView.java
@@ -1,24 +1,12 @@
 package ru.thedevs.coreui.view.unit;
 
-import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.router.Route;
-import io.jmix.core.DataManager;
-import io.jmix.core.Messages;
-import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.textfield.TypedTextField;
-import io.jmix.flowui.component.valuepicker.EntityPicker;
 import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
-import io.jmix.multitenancy.entity.Tenant;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import ru.thedevs.entity.AdditionalData;
-import ru.thedevs.entity.Token;
 import ru.thedevs.entity.Unit;
-import ru.thedevs.entity.embeddable.*;
 
-import java.util.Random;
 
 @Route(value = "units/:id", layout = DefaultMainViewParent.class)
 @ViewController(id = "Unit.detail")
@@ -33,47 +21,47 @@ public class UnitDetailView extends StandardDetailView<Unit> {
     @ViewComponent
     private DataContext dataContext;
 
-    // todo basic stub for entity creation
-    @Subscribe
-    public void onInitEntity(final InitEntityEvent<Unit> event) {
+//     todo basic stub for entity creation
+//    @Subscribe
+//    public void onInitEntity(final InitEntityEvent<Unit> event) {
 
-        Random random = new Random();
-
-        identField.setReadOnly(false);
-//        deviceSF.setVisible(true);
-        nameField.setReadOnly(false);
-
-        Unit created = event.getEntity();
-        created.setTenant("default");
-
-        Token token = dataContext.create(Token.class);
-        token.setSystem(false);
-        token.setName("secretToken");
-        token.setToken("TokenString");
-        created.setToken(token);
-
-        AdditionalData additionalData = dataContext.create(AdditionalData.class);
-        Engine engine = dataContext.create(Engine.class);
-        engine.setStatus(true);
-        additionalData.setEngine(engine);
-        created.setAdditional(additionalData);
-
-        Protocol protocol = dataContext.create(Protocol.class);
-        protocol.setId(random.nextLong());
-        created.setProtocol(protocol);
-
-        Device device = dataContext.create(Device.class);
-        device.setName(RandomStringUtils.randomAlphabetic(7));
-        device.setId(random.nextLong());
-        device.setType(dataContext.create(Type.class));
-        created.setDevice(device);
-
-        Position position = dataContext.create(Position.class);
-        position.setLongitude(random.nextDouble(50, 70));
-        position.setLatitude(random.nextDouble(25, 40));
-        position.setValid(true);
-
-        created.setPosition(position);
+//        Random random = new Random();
+//
+//        identField.setReadOnly(false);
+////        deviceSF.setVisible(true);
+//        nameField.setReadOnly(false);
+//
+//        Unit created = event.getEntity();
+//        created.setTenant("default");
+//
+//        Token token = dataContext.create(Token.class);
+//        token.setSystem(false);
+//        token.setName("secretToken");
+//        token.setToken("TokenString");
+//        created.setToken(token);
+//
+//        AdditionalData additionalData = dataContext.create(AdditionalData.class);
+//        Engine engine = dataContext.create(Engine.class);
+//        engine.setStatus(true);
+//        additionalData.setEngine(engine);
+//        created.setAdditional(additionalData);
+//
+//        Protocol protocol = dataContext.create(Protocol.class);
+//        protocol.setId(random.nextLong());
+//        created.setProtocol(protocol);
+//
+//        Device device = dataContext.create(Device.class);
+//        device.setName(RandomStringUtils.randomAlphabetic(7));
+//        device.setId(random.nextLong());
+//        device.setType(dataContext.create(Type.class));
+//        created.setDevice(device);
+//
+//        Position position = dataContext.create(Position.class);
+//        position.setLongitude(random.nextDouble(50, 70));
+//        position.setLatitude(random.nextDouble(25, 40));
+//        position.setValid(true);
+//
+//        created.setPosition(position);
 
 
 //        Tenant tenant = dataContext.create(Tenant.class);
@@ -87,7 +75,7 @@ public class UnitDetailView extends StandardDetailView<Unit> {
 //        } else {
 //            tenantField.setOptionsList(multitenancyUiSupport.getTenantOptions());
 //        }
-    }
+//    }
 
     @Subscribe(id = "unitDc", target = Target.DATA_CONTAINER)
     public void onUnitDcItemChange(InstanceContainer.ItemChangeEvent<Unit> event) {
